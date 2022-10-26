cf86355cfa2483cfcbfd368974f5b3aec9e62081
Denis Junior <denis_jr2001@hotmail.com>
2022-10-26 13:59:17 -0300

    solve cors

--- server/index.js
+++ server/index.js
@@ -4,6 +4,11 @@ const http = require("http")
 const cors = require("cors")
 const { Server } = require("socket.io")
 app.use(cors())
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 const server = http.createServer(app)
 const io = new Server(server, {
